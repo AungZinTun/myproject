4ed4cd80d1f50d81fe5f9278fe01b7e7d571f050
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-02-18 06:54:40 +0630

    cps

--- config/app.php
+++ config/app.php
@@ -140,7 +140,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Laravel\Dusk\DuskServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -163,8 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
-        
         
 
         Collective\Html\HtmlServiceProvider::class,
